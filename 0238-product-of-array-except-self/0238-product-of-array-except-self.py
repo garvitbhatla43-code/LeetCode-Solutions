388a084d1413c8885dc1fff35ddb2bd535a6a8bc
garvitbhatla43-code <garvitbhatla43@gmail.com>
2026-06-26 15:00:01 +0530

    Time: 28 ms (78.37%), Space: 20.2 MB (49.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        prefix=[1]*len(nums)
-        suffix=[1]*len(nums)
+        
         output=[1]*len(nums)
-        for i in range(1,len(nums)):
-            prefix[i]=prefix[i-1]*nums[i-1]
-        for i in range(len(nums)-2,-1,-1):
-            suffix[i]=suffix[i+1]*nums[i+1]
+        prefix=1
         for i in range(len(nums)):
-            output[i]=prefix[i]*suffix[i]
+            output[i]=prefix
+            prefix*=nums[i]
+        suffix=1
+        for i in range(len(nums)-1,-1,-1):
+            output[i]*=suffix
+            suffix*=nums[i]
         return output
